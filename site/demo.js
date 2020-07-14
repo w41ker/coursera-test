d1d9bc2cf1c2d6eb23d1a541fe991e9325459ddf
Akshat Singh <walker@Akshats-MacBook-Pro.local>
2020-07-14 10:54:08 +0530

    My first Page

---
+++ site/demo.js
@@ -0,0 +1,9 @@
+//This is a comment
+document.write("<h1>Hello world from File</h1>");
+document.write("<h2>Hello world from File H2</h2>");
+
+var num = 16;
+var Name = "Akshat";
+var flag = true;
+alert(Name);
+document.write(num);
