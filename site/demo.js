3e1f179e63377cd141b041c70dac0f4a6f095373
Akshat Singh <walker@Akshats-MacBook-Pro.local>
2020-07-18 13:24:42 +0530

    My Phase 2

--- site/demo.js
+++ site/demo.js
@@ -1,9 +1,4 @@
 //This is a comment
-document.write("<h1>Hello world from File</h1>");
-document.write("<h2>Hello world from File H2</h2>");
+var Name = "Hey Siri";
 
-var num = 16;
-var Name = "Akshat";
-var flag = true;
 alert(Name);
-document.write(num);
